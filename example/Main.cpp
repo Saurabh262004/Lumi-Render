1fb2c7dbd2e89d99c1f73c93b83cdbfd5b5e41c3
Saurabh262004 <saurabhrjadhav26@gmail.com>
2026-09-06 10:13:51 +0530

    erm

--- example/Main.cpp
+++ example/Main.cpp
@@ -29,17 +29,46 @@ void fileDropCallback(Window *window, int path_count, const char **paths) {
 	}
 }
 
-Mesh makeQuad() {
+Mesh makeTestQuad() {
 	std::vector<Vertex> vertices = {
-		{ {-0.5f, -0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f} },
-		{ { 0.5f, -0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 0.0f} },
-		{ { 0.5f,  0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f} },
-		{ {-0.5f,  0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f} },
+		{
+			{ -0.5f, -0.5f, 0.0f       }, // position
+			{  0.0f,  0.0f, 1.0f       }, // normal
+			{  1.0f,  0.0f, 0.0f, 1.0f }, // tangent
+			{  0.0f,  0.0f             }  // texCoord
+		},
+		{
+			{ 0.5f, -0.5f, 0.0f       },
+			{ 0.0f,  0.0f, 1.0f       },
+			{ 1.0f,  0.0f, 0.0f, 1.0f },
+			{ 1.0f,  0.0f             }
+		},
+		{
+			{ 0.5f, 0.5f, 0.0f       },
+			{ 0.0f, 0.0f, 1.0f       },
+			{ 1.0f, 0.0f, 0.0f, 1.0f },
+			{ 1.0f, 1.0f             }
+		},
+		{
+			{ -0.5f, 0.5f, 0.0f       },
+			{  0.0f, 0.0f, 1.0f       },
+			{  1.0f, 0.0f, 0.0f, 1.0f },
+			{  0.0f, 1.0f             }
+		},
 	};
 
-	std::vector<std::uint32_t> indices = { 0, 1, 2, 2, 3, 0 };
+	std::vector<std::uint32_t> indices = {
+		0, 1, 2,
+		2, 3, 0
+	};
 
-	return Mesh(vertices.data(), vertices.size(), indices.data(), indices.size(), Vertex::layout());
+	return Mesh(
+		vertices.data(),
+		vertices.size(),
+		indices.data(),
+		indices.size(),
+		Vertex::layout()
+	);
 }
 
 int main() {
@@ -62,13 +91,14 @@ int main() {
 	window.addShader("shader1", "assets/shaders/shader.vert", "assets/shaders/shader.frag");
 	Shader* shader1 = window.getShader("shader1");
 	shader1->addVec3Uniform("lightDir", Vec3{-0.5f, -1.0f, -0.3f});
+	shader1->activateVec3Uniform("lightDir");
 
 	window.addModel("shader1", "3DCam", "model1", "assets/models/pyramid/obj");
 	Model* model1 = window.getModel("shader1", "3DCam", "model1");
 	model1->addInstance({Mat4::translate({0.0f, 1.0f, 0.0f}), {0.7f, 0.0f, 0.5f}});
 	model1->uploadInstances();
 
-	window.addMesh(makeQuad(), "shader1", "2DCam", "testQuad");
+	window.addMesh(makeTestQuad(), "shader1", "2DCam", "testQuad");
 	Mesh* testQuad = window.getMesh("shader1", "2DCam", "testQuad");
 	testQuad->setTexture(Texture("assets/textures/checker.png"));
 	testQuad->addInstance({ Mat4::translate({-540.0f, 260.0f, 0.0f}) * Mat4::rotateX(0) * Mat4::scale({200.0f, 200.0f, 1.0f}), {1.0f, 1.0f, 1.0f} });
