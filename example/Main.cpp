3ea0118735440d4f7829be9277b3895a0e3253b1
Saurabh262004 <saurabhrjadhav26@gmail.com>
2026-09-07 19:24:04 +0530

    .

--- example/Main.cpp
+++ example/Main.cpp
@@ -104,7 +104,7 @@ int main() {
 
 	window.addMesh(makeTestQuad(), "shader1", "2DCam", "testQuad");
 	Mesh* testQuad = window.getMesh("shader1", "2DCam", "testQuad");
-	//testQuad->setTexture(Texture("assets/textures/checker.png"));
+	testQuad->setTexture(Texture("assets/textures/checker.png"));
 	testQuad->addInstance({ Mat4::translate({-540.0f, 260.0f, 0.0f}) * Mat4::rotateX(0) * Mat4::scale({200.0f, 200.0f, 1.0f}), {1.0f, 1.0f, 1.0f} });
 	testQuad->uploadInstances();
 
